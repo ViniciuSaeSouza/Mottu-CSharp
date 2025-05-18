6fd1c3d2d88f8bede9f76b612064b65506617ab9
ViniciuSaeSouza <viniciusaesouza@gmail.com>
2025-05-18 16:38:15 -0300

    update dirs

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03c51b4f634519fe1f2eb1a319c936e7583c4baf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+689357d4295304d74edd7d17a9c863e179041efe")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
