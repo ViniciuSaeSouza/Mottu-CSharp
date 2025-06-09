0d2d2189f4519c758022d969868612f908d807e0
ViniciuSaeSouza <viniciusaesouza@gmail.com>
2025-06-08 22:14:35 -0300

    up

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4227fb3e3d367dd52f727a40c8bbe56e2262cd41")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35e0cb01d2fc5e73c06d18510e7922e493865dcb")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
