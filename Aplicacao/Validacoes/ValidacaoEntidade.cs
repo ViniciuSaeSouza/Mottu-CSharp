04c805f4b48f2d3a5e591033e27fcba802837bb0
MariaEdPaixao <mariaeduardaalvesdapaixao0807@gmail.com>
2025-09-11 17:13:32 -0300

    feat: entity validation, throw if null

---
+++ Aplicacao/Validacoes/ValidacaoEntidade.cs
@@ -0,0 +1,18 @@
+﻿using Dominio.Excecao;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplicacao.Validacoes
+{
+    public class ValidacaoEntidade
+    {
+        public static void LancarSeNulo<T>(T entidade, string nomeEntidade, object id)
+        {
+            if (entidade == null)
+                throw new EntidadeNaoEncontradaException(nomeEntidade, id);
+        }
+    }
+}
