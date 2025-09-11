e2b4e9c088cbba475db9bf7daca9a39896a2c7e0
Laura-Cintra <lauraocintra@gmail.com>
2025-09-11 08:51:47 -0300

    refactor: motorcycle list opcional in FilialLeituraDTO

--- Aplicacao/DTOs/Filial/FilialLeituraDto.cs
+++ Aplicacao/DTOs/Filial/FilialLeituraDto.cs
@@ -10,13 +10,13 @@ public class FilialLeituraDto
     public string Endereco { get; set; }
 
     // TODO: Deixar lista de motos opcional
-    public ICollection<MotoLeituraDto> Motos { get; set; } = new List<MotoLeituraDto>();
-    public FilialLeituraDto(int id, string nome, string endereco, ICollection<MotoLeituraDto> motos)
+    public ICollection<MotoLeituraDto>? Motos { get; set; }
+    public FilialLeituraDto(int id, string nome, string endereco, ICollection<MotoLeituraDto>? motos)
     {
         Id = id;
         Nome = nome;
         Endereco = endereco;
-        Motos = motos;
+        Motos = motos ?? [];
     }
 
     public FilialLeituraDto() { }
